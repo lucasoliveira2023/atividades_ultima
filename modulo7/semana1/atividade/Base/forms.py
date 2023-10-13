91fe13b79cb2d9f346f37fe31512c11b199570fa
lucasoliveira2023 <126571905+lucasoliveira2023@users.noreply.github.com>
2023-10-13 08:07:21 -0400

    Delete modulo7/semana1 directory

--- modulo7/semana1/atividade/Base/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms 
-
-
-class InscreverForm(forms.Form):
-    
-    nome = forms.CharField(label='Nome')
-    email= forms.EmailField(label='E-mail')
-    observacoes = forms.CharField(label='observacoes', widget=forms.Textarea)
-    
